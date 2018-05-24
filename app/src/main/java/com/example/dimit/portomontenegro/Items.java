c709e51f24ede952f1ff4e47da6a32ddd90b2bf0
Dimitar Bojkovski <dimitar.bojkovski@hotmail.com>
2018-05-24 17:47:09 +0200

    Merge branch 'master' of https://github.com/vlatkosemos/PortoMontenegro2
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/example/dimit/portomontenegro/Items.java
+++ app/src/main/java/com/example/dimit/portomontenegro/Items.java
@@ -1,33 +1,45 @@
 package com.example.dimit.portomontenegro;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.ListView;
 
 import com.example.dimit.portomontenegro.adapters.CustomGridView3;
 import com.example.dimit.portomontenegro.adapters.Item;
+import com.example.dimit.portomontenegro.adapters.ListEventAdapter;
+import com.example.dimit.portomontenegro.objects.ZEvents;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Items extends Activity {
 
+    Context context;
     public CustomGridView3 customGridAdapter;
     ArrayList<Item> gridArray = new ArrayList<Item>();
+
+    ListEventAdapter customAdapter;
+    List<ZEvents> lstEvenets = new ArrayList<>();
+    GridView lstData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+        context=this;
 
-        GridView gridView = (GridView) findViewById(R.id.gridview);
+        lstData = (GridView) findViewById(R.id.gridview);
 
-        Item i1 = new Item("http://demo6.semos.com.mk/assets/images/logonov.png");
-        i1.title="http://demo6.semos.com.mk/assets/images/logonov.png";
-        gridArray.add(i1);
-        gridArray.add(i1);
-        gridArray.add(i1);
+        lstEvenets.add(new ZEvents("Sting", "http://demo8.semos.com.mk/imagesPort/sting.jpg","Concert"));
+        lstEvenets.add(new ZEvents("Hamlet","http://demo8.semos.com.mk/imagesPort/hamlet.jpg","Theatre Show"));
+        lstEvenets.add(new ZEvents("Star Wars: The Last Jedi","http://demo8.semos.com.mk/imagesPort/wars.jpeg","Movie"));
+        lstEvenets.add(new ZEvents("2 Pac", "http://demo8.semos.com.mk/imagesPort/pac.jpg","Concert"));
+        lstEvenets.add(new ZEvents("Arctic Monkeys", "http://demo8.semos.com.mk/imagesPort/monkeys.jpg","Concert"));
 
-        customGridAdapter = new CustomGridView3(this, R.layout.row_gird_readingdigital, gridArray);
-        gridView.setAdapter(customGridAdapter);
+        customAdapter = new ListEventAdapter(context,R.layout.concert_layout, lstEvenets);
+        lstData.setAdapter(customAdapter);
+
     }
 }
