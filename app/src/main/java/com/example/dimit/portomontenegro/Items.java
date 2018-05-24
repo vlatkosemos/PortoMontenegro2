9e45f9599f0b8384ce6ec17b70cec2b64d9b98d1
vlatko.bojkovski <vlatko.bojkovski@studentpartner.com>
2018-05-24 16:55:55 +0200

    Dodadeni stvari za artikli

---
+++ app/src/main/java/com/example/dimit/portomontenegro/Items.java
@@ -0,0 +1,13 @@
+package com.example.dimit.portomontenegro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Items extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+    }
+}
