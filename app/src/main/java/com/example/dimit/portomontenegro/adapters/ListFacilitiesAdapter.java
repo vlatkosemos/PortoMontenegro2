f2343a6bce8c5f12c731e14fb75619f9f544d521
Stefan-pc-private <stefan.ristovski@semos.com.mk>
2018-05-25 00:22:44 +0200

    da

--- app/src/main/java/com/example/dimit/portomontenegro/adapters/ListFacilitiesAdapter.java
+++ app/src/main/java/com/example/dimit/portomontenegro/adapters/ListFacilitiesAdapter.java
@@ -107,7 +107,7 @@ public class ListFacilitiesAdapter  extends ArrayAdapter<ZFacilities> {
                         }
                     };
                 }
-                
+
             }
 
         }
